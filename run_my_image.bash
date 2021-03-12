9e68538aeafa7e120dd1b09a2609a8438f932a02
v <v@computor>
2021-03-11 22:15:32 -0300

    Fixed docker issues with X server
    	modified:   docker-compose.yaml
    	modified:   docker/Dockerfile.app
    	modified:   docker/Dockerfile.motion
    	modified:   run_my_image.bash
    	deleted:    setup_image.bash

--- run_my_image.bash
+++ run_my_image.bash
@@ -1,22 +1,8 @@
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
-fi
+XAUTH_LIST=$(xauth list)
 
-docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
-    --runtime=nvidia \
-    my_melodic_image \
-    bash
+docker run -ti \
+       --net=host \
+       -e DISPLAY \
+       -v /tmp/.X11-unix \
+       --runtime=nvidia \
+       morse_simulation_morse bash -c "xauth add $XAUTH_LIST && bash"
